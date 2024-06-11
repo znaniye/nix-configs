8c2760873aeaafd9d4e31e328afcad9298b4b136
znaniye <zn4niye@proton.me>
2024-06-11 14:09:59 -0300

    working

--- home/programs/nvim/config.lua
+++ home/programs/nvim/config.lua
@@ -1,5 +1,6 @@
 vim.cmd('colorscheme gruvbox')
 
+vim.g.mapleader = ','
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -11,6 +12,3 @@ vim.api.nvim_set_keymap('n', '<C-l>', ':bnext<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-h>', ':bprevious<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-y>', ':ToggleTerm direction=float size=40<CR>', { noremap = true })
 
-
-local function run_script()
-end
--- home/programs/nvim/lsp.nix
+++ home/programs/nvim/lsp.nix
@@ -14,6 +14,20 @@ let
 in
 {
   programs.neovim.plugins = with pkgs.vimPlugins; [
+    {
+      plugin = neodev-nvim;
+      type = "lua";
+      config = ''
+        require("neodev").setup({
+        override = function(root_dir, library)
+        if root_dir:find("/etc/nixos", 1, true) == 1 then
+          library.enabled = true
+          library.plugins = true
+        end
+      end,
+        })
+      '';
+    }
     # LSP
     {
       plugin = nvim-lspconfig;

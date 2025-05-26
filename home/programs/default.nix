2b00f95be29e535d74f69629e927dbf58327d4c4
znaniye <zn4niye@proton.me>
2025-05-26 09:41:29 -0300

    .

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,12 +1,12 @@
 {
   imports = [
-    ./vscode
+    #./vscode
     ./alacritty.nix
     ./i3
     ./zsh.nix
     ./git.nix
-    ./zathura
-    ./emacs
+    #./zathura
+    #./emacs
     ./nvim
   ];
 }

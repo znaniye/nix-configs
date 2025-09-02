5ea69202ff090b78c3898218d42b6c4515f721a9
znaniye <zn4niye@proton.me>
2025-09-02 16:30:20 -0300

    add vm, add wormhole

--- modules/home-manager/cli/default.nix
+++ modules/home-manager/cli/default.nix
@@ -22,7 +22,7 @@ in
   config = lib.mkIf cfg.enable {
     home = {
       packages = with pkgs; [
-
+        magic-wormhole
         fastfetch
         unzip
         tokei
--- modules/nixos/desktop/virtualization.nix
+++ modules/nixos/desktop/virtualization.nix
@@ -3,13 +3,28 @@
   lib,
   ...
 }:
+let
+  cfg = config.nixos.desktop.virtualization;
+in
 {
 
-  options.nixos.desktop.virtualization.enable = lib.mkEnableOption "virtualization cfg" // {
-    default = false;
+  options.nixos.desktop.virtualization = {
+    enable = lib.mkEnableOption "virtualization cfg" // {
+      default = false;
+    };
+
+    vmConfig = lib.mkOption {
+      type = lib.types.attrs;
+      description = "Virtualization options.";
+      default = {
+        memorySize = 4096;
+        cores = 4;
+      };
+    };
+
   };
 
-  config = lib.mkIf config.nixos.desktop.virtualization.enable {
+  config = lib.mkIf cfg.enable {
     boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
     virtualisation.libvirtd.enable = true;
@@ -17,5 +32,10 @@
 
     users.users.${config.meta.username}.extraGroups =
       lib.optional config.nixos.desktop.virtualization.enable "libvirtd"; # ++
+
+    virtualisation = {
+      vmVariant.virtualisation = cfg.vmConfig;
+      vmVariantWithBootLoader.virtualisation = cfg.vmConfig;
+    };
   };
 }

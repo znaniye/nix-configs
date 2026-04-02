3ff9900aeb0abb10dc4922934441a61de1972b81
Samuel Silva <samuel@ossystems.com.br>
2026-04-02 10:46:23 -0300

    feat(gitea): add self-hosted actions runner support
    Expose runner options and wire gitea-actions-runner so this host can execute Actions jobs locally with explicit token-file registration.

--- modules/nixos/server/gitea.nix
+++ modules/nixos/server/gitea.nix
@@ -5,22 +5,77 @@
   pkgs,
   ...
 }:
+let
+  cfg = config.nixos.server.gitea;
+in
 
 {
 
-  options.nixos.server.gitea.enable = lib.mkEnableOption "gitea config" // {
-    default = config.nixos.server.enable;
+  options.nixos.server.gitea = {
+    enable = lib.mkEnableOption "gitea config" // {
+      default = config.nixos.server.enable;
+    };
+
+    runner = {
+      enable = lib.mkEnableOption "self-hosted Gitea Actions runner";
+
+      name = lib.mkOption {
+        type = lib.types.str;
+        default = config.networking.hostName;
+        description = "Name used when registering the runner in Gitea.";
+      };
+
+      url = lib.mkOption {
+        type = lib.types.str;
+        default = "http://127.0.0.1:3000";
+        description = "Base URL of the Gitea instance used by the runner.";
+      };
+
+      tokenFile = lib.mkOption {
+        type = lib.types.nullOr (lib.types.either lib.types.str lib.types.path);
+        default = null;
+        example = "/run/secrets/gitea-runner.env";
+        description = "Environment file containing TOKEN=<registration-token>.";
+      };
+
+      labels = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ "native:host" ];
+        description = "Runner labels used by workflows in runs-on.";
+      };
+    };
   };
 
-  config = lib.mkIf config.nixos.server.gitea.enable {
+  config = lib.mkIf cfg.enable {
+
+    assertions = [
+      {
+        assertion = !(cfg.runner.enable && cfg.runner.tokenFile == null);
+        message = "Set nixos.server.gitea.runner.tokenFile when nixos.server.gitea.runner.enable is true.";
+      }
+    ];
 
     users.users.gitea.openssh.authorizedKeys.keys = myAuthorizedKeys;
 
     services.gitea = {
       enable = true;
-      settings.server = {
-        SSH_PORT = 2222;
-        START_SSH_SERVER = true;
+      settings = {
+        server = {
+          SSH_PORT = 2222;
+          START_SSH_SERVER = true;
+        };
+
+        actions.ENABLED = true;
+      };
+    };
+
+    services.gitea-actions-runner.instances = lib.mkIf cfg.runner.enable {
+      local = {
+        enable = true;
+        name = cfg.runner.name;
+        url = cfg.runner.url;
+        tokenFile = cfg.runner.tokenFile;
+        labels = cfg.runner.labels;
       };
     };
 

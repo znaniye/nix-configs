4bf7e7f99a864fff4675d8634f820acf84726be4
Samuel Silva <samuel@ossystems.com.br>
2026-07-28 19:35:00 -0300

    feat(massan): authorize znaniye@golf ssh key

--- hosts/darwin/massan/default.nix
+++ hosts/darwin/massan/default.nix
@@ -6,6 +6,10 @@
   darwin.wireguard.enable = true;
   darwin.openssh.enable = true;
 
+  users.users.znaniye.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMbJhk5H0h7Oi79LSHLWfuffv6uFcuXtm77kewxrwQsD znaniye@golf"
+  ];
+
   darwin.home.extraModules = {
     home-manager.editor.enable = true;
     home-manager.desktop.herdr.enable = true;
